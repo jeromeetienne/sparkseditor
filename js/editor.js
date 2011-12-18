0449753f98c6721112e2afdda386718dca6657f1
Jerome Etienne <jerome.etienne@gmail.com>
2011-12-18 14:11:33 +0100

    modified:   js/editor.js

--- js/editor.js
+++ js/editor.js
@@ -1,61 +1,6 @@
-var value = ""+
-"threexSparks	= new THREEx.Sparks({\n"+
-"	maxParticles	: 400,\n"+
-"	counter		: new SPARKS.SteadyCounter(300)\n"+
-"});\n"+
-"\n"+
-"// setup the emitter\n"+
-"var emitter	= threexSparks.emitter();\n"+
-"\n"+
-"var initColorSize	= function(){\n"+
-"	this.initialize = function( emitter, particle ){\n"+
-"		particle.target.color().setHSV(0.3, 0.9, 0.4);\n"+
-"		particle.target.size(150);\n"+
-"	};\n"+
-"};\n"+
-"\n"+
-"\n"+
-"emitter.addInitializer(new initColorSize());\n"+
-"emitter.addInitializer(new SPARKS.Position( new SPARKS.PointZone( new THREE.Vector3(0,0,0) ) ) );\n"+
-"emitter.addInitializer(new SPARKS.Lifetime(0,0.8));\n"+
-"emitter.addInitializer(new SPARKS.Velocity(new SPARKS.PointZone(new THREE.Vector3(0,250,00))));\n"+
-"\n"+
-"emitter.addAction(new SPARKS.Age());\n"+
-"emitter.addAction(new SPARKS.Move());\n"+
-"emitter.addAction(new SPARKS.RandomDrift(1000,0,1000));\n"+
-"emitter.addAction(new SPARKS.Accelerate(0,-200,0));\n"+
-"\n";
-
-
-
-var updateText= function(){
-	if( threexSparks ){
-		scene.remove(threexSparks.container());
-		threexSparks.destroy();
-		threexSparks	= null;
-	}
-	
-	var textValue	= editor.getValue();
-	eval(textValue);
-
-	// restart it 
-	threexSparks.emitter().start();	
-	scene.add(threexSparks.container());
-	
-	optionsSave();
-}
-
-var editor	= CodeMirror(document.getElementById('editor'), {
-	value		: value,
-	mode		: "javascript",
-	theme		: 'night',
-
-	indentUnit	: 8,
-	tabSize		: 8,
-	indentWithTabs	: true,
-	
-	onChange	: updateText
-});
+//////////////////////////////////////////////////////////////////////////////////
+//										//
+//////////////////////////////////////////////////////////////////////////////////
 
 var optionsSave	= function(){
 	console.log("Saving options to url...")
@@ -100,6 +45,77 @@ var optionsLoad	= function(){
 		camera.rotation.z	= options.camera.rotation.z; 
 	}
 }
+
+//////////////////////////////////////////////////////////////////////////////////
+//										//
+//////////////////////////////////////////////////////////////////////////////////
+
+// default effect
+var value = ""+
+"threexSparks	= new THREEx.Sparks({\n"+
+"	maxParticles	: 400,\n"+
+"	counter		: new SPARKS.SteadyCounter(300)\n"+
+"});\n"+
+"\n"+
+"// setup the emitter\n"+
+"var emitter	= threexSparks.emitter();\n"+
+"\n"+
+"var initColorSize	= function(){\n"+
+"	this.initialize = function( emitter, particle ){\n"+
+"		particle.target.color().setHSV(0.3, 0.9, 0.4);\n"+
+"		particle.target.size(150);\n"+
+"	};\n"+
+"};\n"+
+"\n"+
+"\n"+
+"emitter.addInitializer(new initColorSize());\n"+
+"emitter.addInitializer(new SPARKS.Position( new SPARKS.PointZone( new THREE.Vector3(0,0,0) ) ) );\n"+
+"emitter.addInitializer(new SPARKS.Lifetime(0,0.8));\n"+
+"emitter.addInitializer(new SPARKS.Velocity(new SPARKS.PointZone(new THREE.Vector3(0,250,00))));\n"+
+"\n"+
+"emitter.addAction(new SPARKS.Age());\n"+
+"emitter.addAction(new SPARKS.Move());\n"+
+"emitter.addAction(new SPARKS.RandomDrift(1000,0,1000));\n"+
+"emitter.addAction(new SPARKS.Accelerate(0,-200,0));\n"+
+"\n";
+
+//////////////////////////////////////////////////////////////////////////////////
+//										//
+//////////////////////////////////////////////////////////////////////////////////
+
+var updateText= function(){
+	if( threexSparks ){
+		scene.remove(threexSparks.container());
+		threexSparks.destroy();
+		threexSparks	= null;
+	}
+	
+	var textValue	= editor.getValue();
+	eval(textValue);
+
+	// restart it 
+	threexSparks.emitter().start();	
+	scene.add(threexSparks.container());
+	
+	optionsSave();
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+//										//
+//////////////////////////////////////////////////////////////////////////////////
+
+var editor	= CodeMirror(document.getElementById('editor'), {
+	value		: value,
+	mode		: "javascript",
+	theme		: 'night',
+
+	indentUnit	: 8,
+	tabSize		: 8,
+	indentWithTabs	: true,
+	
+	onChange	: updateText
+});
+
 
 optionsLoad();
 jQuery("#editor").hide();
